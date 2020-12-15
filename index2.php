61da124eabfb79cc2bfd5c8ae8b1fd0fdd1066ab
chawakorner <76036927+chawakorner@users.noreply.github.com>
2020-12-15 16:21:56 +0700

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <title>ทดสอบ HTML</title>
+  </head>
+  <body>
+    <h1>สวัสดี ศิลปากร</h1>
+    <h2>Web technology for Data Sciencetist</h2>
+
+<p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตร์บัณฑิต สาขาวิชาวิทยาการข้อมูล
+ภาษาอังกฤษ :  Bachelor of Science Program in Data Science
+ชื่อปริญญา
+ภาษาไทย : วิทยาศาสตรบัณฑิต (วิทยาการข้อมูล)
+ภาษาอังกฤษ : Bachelor of Science (Data Science)</p>
+
+<a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตรวิทยาการข้อมุล</a>
+<img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.fcbarcelona.com%2Fen%2Fnews%2F1349836%2Fall-you-need-to-know-about-ansu-fati&psig=AOvVaw0jTaK_fD7u5guyoDSdyT80&ust=1608110428530000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCIDy-K7Uz-0CFQAAAAAdAAAAABAD.jpg" width="500" height="600">
+
+  </body>
+</html>
