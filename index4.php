9212f4ee33c576458d3104cdacc28acca6e9d5cb
chawakorner <76036927+chawakorner@users.noreply.github.com>
2020-12-15 16:32:33 +0700

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="th">
+  <head>
+    <meta charset="UTF-8">
+    <title>ทดสอบ HTML</title>
+  </head>
+  <body>
+      <h1>สวัสดี ศิลปากร</h1>
+      <h2>web technology for data sciecne</h2>
+
+<p>ชื่อหลักสูตรภาษาไทย : หลักสูตรวิทยาศาสตรบัณฑิต สาขาวิชาวิทยาการข้อมูล</p>
+<p>ภาษาอังกฤษ :Bachelor of Science Program in Data Science</p>
+<p>ชื่อปริญญา</p>
+<p>ภาษาไทย : วิทยาศาสตรบัณฑิต (วิทยาการข้อมูล)</p>
+<p>ภาษาอังกฤษ :Bachelor of Science (Data Science)</p>
+
+<a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">รายละเอียดหลักสูตร วิทยาการข้อมูล</a>
+<img src="https://3.bp.blogspot.com/-vJpCEJvBxaM/XDhMe4B8SCI/AAAAAAAA0cs/rwvjqKZflSQ0herxmUD445aOAy9bXNpJACLcBGAs/s1600/cutetoon%2B%25289%2529.jpeg" width="500" height="600">
+
+  </body>
+<html>
